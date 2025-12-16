8f0b7c91dbb84d6a1f82423b23512ef1c5cdb785
aleksandremdivani <aleksandre.mdivani@geolab.edu.ge>
2025-12-16 20:28:50 +0400

    added products

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const refreshBtn = document.getElementById("refresh-btn");
+refreshBtn.addEventListener("click", function () {
+  window.location.reload();
+});
+let products = [];
+let filteredProducts = [];
+const fetchdata = async () => {
+  try {
+    const response = await fetch("https://dummyjson.com/carts/13");
+    const data = await response.json();
+    products = data.products;
+    filteredProducts = products;
+    renderProducts(filteredProducts);
+  } catch (error) {
+    console.log(error);
+  }
+};
+fetchdata();
+
+const renderProducts = (productsList) => {
+    const productsListContainer = document.getElementById("products-list");
+    productsListContainer.innerHTML = "";
+    productsList.forEach((product) => {
+        const { title, price, quantity, total, thumbnail} = product;
+        const productDiv = document.createElement("div");
+        productDiv.innerHTML = `
+        <img src='${thumbnail}' alt="${title}">
+        <h3>${title}</h3>
+        <p>${price}</p>
+        <p>${quantity}</p>
+        <p>${total}</p>
+        `;
+        productDiv.style.display = "flex";
+        productsListContainer.append(productDiv);
+    });
+};
